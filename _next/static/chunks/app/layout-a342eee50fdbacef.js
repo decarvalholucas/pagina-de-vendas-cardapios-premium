3d50dc8081b44b6d7a046dd0b99ad4fbbeadc1a6
decarvalholucas <lucasdecarvalho.ti@gmail.com>
2023-05-09 14:48:09 -0300

    Primeiro commit

---
+++ _next/static/chunks/app/layout-a342eee50fdbacef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2030:function(n,e,t){Promise.resolve().then(t.t.bind(t,7307,23)),Promise.resolve().then(t.t.bind(t,9888,23))},9888:function(){},7307:function(){}},function(n){n.O(0,[667,139,744],function(){return n(n.s=2030)}),_N_E=n.O()}]);
